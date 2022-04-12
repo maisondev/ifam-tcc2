1190c570a6a8967319eba559acf9a9aa6fae8159
Maison <maisongalvao@outlook.com>
2022-04-12 14:10:23 -0400

    Adicionando o tailwind ao projeto
    Signed-off-by: Maison <maisongalvao@outlook.com>

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link class="bg-red-100 text-2xl" to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
