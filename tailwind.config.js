b855972802db195567c7c89e8d248f40d402ae7e
Maison <maisongalvao@outlook.com>
2022-04-11 22:10:08 -0400

    Primeiro commit
    Signed-off-by: Maison <maisongalvao@outlook.com>

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
+}
