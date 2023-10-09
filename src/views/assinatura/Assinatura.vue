71c311904e7e7fb2e22051281abc5f6c36710971
Maison <maison.souza@prodam.am.gov.br>
2023-10-09 15:07:21 -0400

    Adicionando página de assinatura Adicionando componente básico para botões Editando textos da página de login

--- src/views/assinatura/Assinatura.vue
+++ src/views/assinatura/Assinatura.vue
@@ -3,15 +3,22 @@ import BaseButton from "@/components/base/BaseButton.vue";
 
 export default {
   name: "Assinatura",
-  components: {BaseButton}
+  components: {BaseButton},
+  methods:{
+    getVoltar(){
+      console.log("getVoltar");
+      this.$router.push("/")
+    }
+  }
 }
 </script>
 
 <template>
 <div class="bg-blue-500 rounded h-full">
   <h1 class="text-gray-50 text-2xl p-4">Assinatura</h1>
-  <div class="p-4">
+  <div class="p-4 flex justify-around">
     <BaseButton rotulo="Assinar"></BaseButton>
+    <BaseButton rotulo="Voltar" @action="getVoltar"></BaseButton>
   </div>
 
 </div>
